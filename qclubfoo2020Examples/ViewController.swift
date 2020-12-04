841160467ebb19f74482927eacddaba9624d481f
Dmitrii <dp20121989@mail.ru>
2020-12-04 15:10:28 +0300

    initial project setup

---
+++ qclubfoo2020Examples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qclubfoo2020Examples
+//
+//  Created by Дмитрий on 04.12.2020.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
